ce79b86bec190a5fb7715e44b16aa7b38ed2d3ce
Sporard <pierresabard@gmail.com>
2023-04-24 20:25:36 +0200

    Initial Commit

---
+++ OpenQuizz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenQuizz
+//
+//  Created by Pierre Sabard on 24/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
