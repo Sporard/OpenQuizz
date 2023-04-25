fa0830fb48185d8bb72ecd1cabc345829baa8650
Sporard <pierresabard@gmail.com>
2023-04-26 00:14:32 +0200

    add game logique and callback

---
+++ OpenQuizz/Model/Game.swift
@@ -0,0 +1,58 @@
+//
+//  Game.swift
+//  OpenQuizz
+//
+//  Created by Pierre Sabard on 25/04/2023.
+//
+
+import Foundation
+
+class Game {
+    var score: Int = 0
+    var questions: [Question] = []
+    
+    enum State {
+      case ongoing, over
+    }
+
+    var state: State = State.ongoing
+    
+    private var currentIndex: Int = 0;
+    
+    var currentQuestion: Question {
+        get {
+            return self.questions[self.currentIndex]
+        }
+    }
+    private func goToNextQuestion() {
+        if currentIndex < questions.count - 1 {
+            currentIndex += 1
+        } else {
+            finishGame()
+        }
+    }
+    private func finishGame() {
+        state = .over
+    }
+    
+    func answerCurrentQuestion(with answer: Bool){
+        if (answer == self.currentQuestion.isCorrect){
+            self.score += 1
+            self.goToNextQuestion()
+        }
+    }
+    
+    func refresh () {
+        self.score = 0
+        self.currentIndex = 0
+        self.state = State.over
+        
+        QuestionManager.shared.get(completionHandler: receiveQuestions)
+        
+    }
+    
+    private func receiveQuestions(_ questions: [Question]){
+        self.questions = questions
+        self.state = State.ongoing
+    }
+}
---
+++ OpenQuizz/Model/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  OpenQuizz
+//
+//  Created by Pierre Sabard on 25/04/2023.
+//
+
+import Foundation
+
+struct Question {
+    var title: String = ""
+    var isCorrect: Bool = false
+}
